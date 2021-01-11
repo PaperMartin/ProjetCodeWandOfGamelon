4db89d38a74b18cac9830d3e13d181bfb7f9e98b
SeedHolt <pierre.jeanneholt@gmail.com>
2021-01-11 11:43:18 +0100

    Premier Script de la Caméra Pixel Perfect + Création de ma scène de test + création du préfab de la caméra

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform targetPlayer;
+
+    private Vector3 lastPositionX;
+    private Vector3 posX;
+    private Vector3 lastPositionY;
+    private Vector3 posY;
+
+    public float largeur;
+    public float hauteur;
+
+    public float travelDuration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdatePosition();
+        lastPositionX = posX;
+        lastPositionY = posY;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdatePosition();
+
+        if (lastPositionX != posX)
+        {
+            transform.DOMoveX(largeur * posX.x, travelDuration).SetEase(Ease.OutCubic);
+            lastPositionX = posX;
+        }
+
+        if (lastPositionY != posY)
+        {
+            transform.DOMoveY(hauteur * posY.y, travelDuration).SetEase(Ease.OutCubic);
+            lastPositionY = posY;
+        }
+    }
+
+    void UpdatePosition()
+    {
+        posX = targetPlayer.position;
+        posX.x = Mathf.FloorToInt(posX.x / largeur);
+
+        posY = targetPlayer.position;
+        posY.y = Mathf.FloorToInt(posY.y / hauteur);
+    }
+}
