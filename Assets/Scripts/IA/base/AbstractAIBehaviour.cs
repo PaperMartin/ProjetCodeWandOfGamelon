914e4c03bcc95d8fbd64247d8a171a7857a12243
PaperMartin <martin.wetischek@orange.fr>
2021-01-14 14:31:58 +0100

    FSM

---
+++ Assets/Scripts/IA/base/AbstractAIBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AbstractAIBehaviour : MonoBehaviour
+{
+
+    // Must return the short hash value of the Animator state corresponding to this behaviour.
+    abstract public int GetBehaviourHash();
+
+    virtual public void onEnterState() 
+    {
+        //Debug.Log("on enter state");
+    }
+
+    virtual public void onExitState()
+    {
+        //Debug.Log("on exit state");
+    }
+
+    // OnEnable()
+    // OnDisable()
+    // enable = true/false;
+}
