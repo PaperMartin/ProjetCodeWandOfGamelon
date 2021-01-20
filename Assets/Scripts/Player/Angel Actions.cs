2ef5cd9c60b52f68744e5367e93b7028e0f2840e
SeedHolt <pierre.jeanneholt@gmail.com>
2021-01-20 23:41:34 +0100

    On avance sur ce Character Controller
    J'ai une petite hésitation au niveau de la méthode à employer au départ, mais quand ça, ça sera passé, ça *devrait* aller, plus ou moins

---
+++ Assets/Scripts/Player/Angel Actions.cs
@@ -0,0 +1,260 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/IA/Player/Angel Actions.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @AngelActions : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @AngelActions()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""Angel Actions"",
+    ""maps"": [
+        {
+            ""name"": ""Base"",
+            ""id"": ""10ec6dc9-2969-4baf-8800-2e69fc4ae9a0"",
+            ""actions"": [
+                {
+                    ""name"": ""Walk"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""8de8efa6-09c4-4a56-923c-f61f86c102cc"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Attack"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""43789484-c3eb-4dcc-8fd4-8f9b4b1c86dd"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""Walk"",
+                    ""id"": ""632874b0-a5ab-4c80-89f7-34afeceb4a64"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Walk"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""81990653-8a4f-4c6e-b906-1f5e4877f5e3"",
+                    ""path"": ""<Keyboard>/upArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Walk"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""db8156b3-8026-46e6-bc19-a9ea8018d09f"",
+                    ""path"": ""<Keyboard>/z"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Walk"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""e72b4324-a5cd-4006-abcf-d1e2601e43eb"",
+                    ""path"": ""<Keyboard>/downArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Walk"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""26aebf2d-673c-4fb3-9e7e-c08dcc3794d2"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Walk"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""3181c098-0eb2-4627-84ee-a3cf1ab56117"",
+                    ""path"": ""<Keyboard>/leftArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Walk"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""27f41727-c445-4eab-bb7d-33d9b4526f11"",
+                    ""path"": ""<Keyboard>/q"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Walk"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""173ffa06-0e81-44cb-b47c-01ef8f59b4aa"",
+                    ""path"": ""<Keyboard>/rightArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Walk"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""cab342e5-ff07-47d1-bbb4-e1af703f13cb"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Walk"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""2eef6dfe-5a67-48fb-9f15-e5f19648b568"",
+                    ""path"": ""<Mouse>/leftButton"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Attack"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""606e4320-4680-424d-86c8-8ef523c4d4ec"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Attack"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Base
+        m_Base = asset.FindActionMap("Base", throwIfNotFound: true);
+        m_Base_Walk = m_Base.FindAction("Walk", throwIfNotFound: true);
+        m_Base_Attack = m_Base.FindAction("Attack", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Base
+    private readonly InputActionMap m_Base;
+    private IBaseActions m_BaseActionsCallbackInterface;
+    private readonly InputAction m_Base_Walk;
+    private readonly InputAction m_Base_Attack;
+    public struct BaseActions
+    {
+        private @AngelActions m_Wrapper;
+        public BaseActions(@AngelActions wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Walk => m_Wrapper.m_Base_Walk;
+        public InputAction @Attack => m_Wrapper.m_Base_Attack;
+        public InputActionMap Get() { return m_Wrapper.m_Base; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(BaseActions set) { return set.Get(); }
+        public void SetCallbacks(IBaseActions instance)
+        {
+            if (m_Wrapper.m_BaseActionsCallbackInterface != null)
+            {
+                @Walk.started -= m_Wrapper.m_BaseActionsCallbackInterface.OnWalk;
+                @Walk.performed -= m_Wrapper.m_BaseActionsCallbackInterface.OnWalk;
+                @Walk.canceled -= m_Wrapper.m_BaseActionsCallbackInterface.OnWalk;
+                @Attack.started -= m_Wrapper.m_BaseActionsCallbackInterface.OnAttack;
+                @Attack.performed -= m_Wrapper.m_BaseActionsCallbackInterface.OnAttack;
+                @Attack.canceled -= m_Wrapper.m_BaseActionsCallbackInterface.OnAttack;
+            }
+            m_Wrapper.m_BaseActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Walk.started += instance.OnWalk;
+                @Walk.performed += instance.OnWalk;
+                @Walk.canceled += instance.OnWalk;
+                @Attack.started += instance.OnAttack;
+                @Attack.performed += instance.OnAttack;
+                @Attack.canceled += instance.OnAttack;
+            }
+        }
+    }
+    public BaseActions @Base => new BaseActions(this);
+    public interface IBaseActions
+    {
+        void OnWalk(InputAction.CallbackContext context);
+        void OnAttack(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/Scripts/Player/AngelController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AngelController : MonoBehaviour
+{
+
+    private AngelActions _angelActions;
+
+    private void Awake()
+    {
+        _angelActions = new AngelActions();
+    }
+
+    private void OnDisable()
+    {
+        _angelActions.Disable();
+    }
+
+    private void OnEnable()
+    {
+        _angelActions.Enable();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        _angelActions.Base.Walk.ReadValue<Vector2>();
+    }
+}
