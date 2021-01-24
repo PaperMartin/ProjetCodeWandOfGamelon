5344d4b00e77ec9a9291f85b3dd6c55ea6db5a5f
PaperMartin <martin.wetischek@orange.fr>
2021-01-24 21:47:55 +0100

    IA + Spawn TP Script

---
+++ Assets/Scripts/Enemy1/GoBackToSpawn.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoBackToSpawn : AbstractAIBehaviour
+{
+    public string behaviorHash;
+    
+    private Vector2 Spawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Spawn = transform.position;
+    }
+
+    public override int GetBehaviourHash()
+    {
+        return Animator.StringToHash(behaviorHash);
+    }
+
+    public override void onEnterState()
+    {
+        transform.position = Spawn;
+    }
+
+    public override void onExitState()
+    {
+        
+    }
+}
