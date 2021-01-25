fef9a5f41423f09ab5e2521b6bd672905e9f6166
PaperMartin <martin.wetischek@orange.fr>
2021-01-25 10:18:49 +0100

    Damage and death event

--- Assets/Scripts/Player/Health.cs
+++ Assets/Scripts/Player/Health.cs
@@ -1,28 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Health : MonoBehaviour
 {
     public int hp = 2;
+
+    public UnityEvent OnDamage;
+    public UnityEvent OnDeath;
 
     private bool selfSwitchA;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void LoseHealth()
     {
         hp -= 1;
+        OnDamage.Invoke();
         if (hp == 0)
         {
             Death();
@@ -31,6 +36,6 @@ public class Health : MonoBehaviour
 
     void Death()
     {
-        
+        OnDeath.Invoke();
     }
 }
