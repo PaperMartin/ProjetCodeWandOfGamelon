9d251d5111281c644118a699737a647a7ffc17b3
PaperMartin <martin.wetischek@orange.fr>
2021-01-25 10:43:07 +0100

    Stun script

---
+++ Assets/Scripts/Enemy1/Stun.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stun : AbstractAIBehaviour
+{
+
+    public string behaviorHash;
+    [SerializeField]
+    private Rigidbody2D rbody2D;
+    [SerializeField]
+    private float StunMoveSpeed;
+
+    Vector2 MoveDir;
+
+    private bool IsActiveState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public override int GetBehaviourHash()
+    {
+        return Animator.StringToHash(behaviorHash);
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (IsActiveState == true){
+            rbody2D.velocity =  MoveDir * StunMoveSpeed;
+        }
+    }
+
+    public override void onEnterState()
+    {
+        IsActiveState = true;
+    }
+
+    public override void onExitState()
+    {
+        IsActiveState = false;
+    }
+
+    public void UpdateMoveDir()
+    {
+
+    }
+}
