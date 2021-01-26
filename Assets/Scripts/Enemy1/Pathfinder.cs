42f5124e79314c4e31e16b40a9fe55a5e56896e9
PaperMartin <martin.wetischek@orange.fr>
2021-01-26 17:18:43 +0100

    pathfinder

---
+++ Assets/Scripts/Enemy1/Pathfinder.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour
+{
+    public bool CanWalkUp(){
+        return true;
+    }
+
+    public bool CanWalkDown(){
+        return true;
+    }
+
+    public bool CanWalkLeft(){
+        return true;
+    }
+
+    public bool CanWalkRight(){
+        return true;
+    }
+}
