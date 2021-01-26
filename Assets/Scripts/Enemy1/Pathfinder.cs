80a85dba8987940ff18d83995a56c7bb611f59c0
PaperMartin <martin.wetischek@orange.fr>
2021-01-26 18:22:53 +0100

    pathfinder fonctionel + follow player

--- Assets/Scripts/Enemy1/Pathfinder.cs
+++ Assets/Scripts/Enemy1/Pathfinder.cs
@@ -5,18 +5,26 @@ using UnityEngine;
 public class Pathfinder : MonoBehaviour
 {
     public bool CanWalkUp(){
-        return true;
+        return CheckDirection(Vector2.up);
     }
 
     public bool CanWalkDown(){
-        return true;
+        return CheckDirection(Vector2.down);
     }
 
     public bool CanWalkLeft(){
-        return true;
+        return CheckDirection(Vector2.left);
     }
 
     public bool CanWalkRight(){
-        return true;
+        return CheckDirection(Vector2.right);
+    }
+
+    private bool CheckDirection(Vector2 direction){
+        RaycastHit2D hit = Physics2D.Raycast(transform.position,direction,1f);
+        if (hit.transform.tag == "Map"){
+            return false;
+        }
+        else return true;
     }
 }
---
+++ Assets/Scripts/SetTargetToPlayer.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetTargetToPlayer : MonoBehaviour
+{
+    [SerializeField]
+    private Walk walk;
+    [SerializeField]
+    private Pathfinder pathfinder;
+    private Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 direction = (player.position - transform.position).normalized;
+        bool walkHorizontal = false;
+        if (Mathf.Abs(direction.x) >= Mathf.Abs(direction.y))
+        {
+            if (pathfinder.CanWalkLeft() || pathfinder.CanWalkRight())
+            {
+                walkHorizontal = true;
+            }
+        }
+        Vector2 DirectionFinal = Vector2.zero;
+
+        if (walkHorizontal == true)
+        {
+            if (direction.x >= 0 && pathfinder.CanWalkRight())
+            {
+                DirectionFinal.x = 1;
+            }
+            else
+            {
+                DirectionFinal.x = -1;
+            }
+        }
+        else
+        {
+            if (direction.y >= 0 && pathfinder.CanWalkUp())
+            {
+                DirectionFinal.y = 1;
+            }
+            else
+            {
+                DirectionFinal.y = -1;
+            }
+        }
+    }
+}
